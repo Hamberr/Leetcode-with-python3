13f7abed9143bae9f85f8a28390164edabe1a51f
Hamberr <40656747+Hamberr@users.noreply.github.com>
2019-12-23 21:33:03 +0800

    Update 300_Longest_Increasing_Subsequence.py

--- 300_Longest_Increasing_Subsequence.py
+++ 300_Longest_Increasing_Subsequence.py
@@ -1,3 +1,9 @@
+"""
+Dynamic programming:
+Creat list dp = [1] * len(nums) to store the present longest increasing subquences: dp[i] is the LIS of nums[0 : i], update dp[i] with dp[i] = max(dp[i], dp[j] + 1 for j in range(i) if nums[j] < nums[i];
+Finally, return max(dp[i])
+"""
+
 class Solution:
     def lengthOfLIS(self, nums):
 
