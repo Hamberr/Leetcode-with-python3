e6729c20a23f8db7a7c6152c58ecdd372bc172d4
Hamberr <40656747+Hamberr@users.noreply.github.com>
2019-12-29 21:18:47 +0800

    Update basic.py

--- Binary_search/basic.py
+++ Binary_search/basic.py
@@ -1,3 +1,12 @@
+"""
+二分查找的适用条件是有序列表~
+最基本的二分查找，返回列表中目标值item的索引，若没有则返回None.
+注意while循环的条件是low<=high，这是由于判断是否存在某一元素实际上是通过list[mid]判断的，
+当low = high - 1时，mid = low = high - 1，判断的实际是list[low]若item = list[high]，则程序进入low = low + 1，此时low == high
+若while循环条件是low < high，则跳出循环返回None，即搜索不到list[high]
+
+由于是查找item是否存在，因此若list[mid]不是，那么不必管它，因此low和high的更新分别为mid + 1和mid - 1
+"""
 def binary_search(list, item):
     
     low = 0
